2be6533b4661d40cc97cc3b42add7b2af95bb720
Akib424 <akibzaman424@gmail.com>
2024-05-19 15:22:21 -0600

    hourly commit

--- app/src/main/java/com/example/kalendo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kalendo/ui/theme/Theme.kt
@@ -280,7 +280,7 @@ fun AppTheme(
       val window = (view.context as Activity).window
       window.statusBarColor = colorScheme.primary.toArgb()
       //window.statusBarColor = Color.Transparent.toArgb()
-      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+      //WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
     }
   }
 
