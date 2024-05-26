ccb612c065b5259c896b9f1c17e201429049ee01
Xuan404 <akibzaman424@gmail.com>
2024-05-26 03:14:42 -0600

    Add Code for Dao interfaces

--- app/src/main/java/com/example/kalendo/data/local/dao/AssignmentDao.kt
+++ app/src/main/java/com/example/kalendo/data/local/dao/AssignmentDao.kt
@@ -1,2 +1,20 @@
 package com.example.kalendo.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.kalendo.data.local.entity.AssignmentEntity
+
+@Dao
+interface AssignmentDao {
+    @Insert
+    suspend fun insert(assignment: AssignmentEntity)
+
+    @Query("DELETE FROM assignments WHERE id = :assignmentId")
+    suspend fun delete(assignmentId: Int)
+
+    @Query("SELECT * FROM assignments WHERE courseId = :courseId")
+    suspend fun getAssignmentsForCourse(courseId: Int): List<AssignmentEntity>
+
+    //Add other Queries as necessary
+}
--- app/src/main/java/com/example/kalendo/data/local/dao/CourseDao.kt
+++ app/src/main/java/com/example/kalendo/data/local/dao/CourseDao.kt
@@ -1,2 +1,20 @@
 package com.example.kalendo.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.kalendo.data.local.entity.CourseEntity
+
+@Dao
+interface CourseDao {
+    @Insert
+    suspend fun insert(course: CourseEntity)
+
+    @Query("DELETE FROM courses WHERE id = :courseId")
+    suspend fun delete(courseId: Int)
+
+    @Query("SELECT * FROM courses")
+    suspend fun getAllCourses(): List<CourseEntity>
+
+    //Add other Queries as necessary
+}
