c3864d6cf65610decbcef08cec9099a4d0881a39
Xuan404 <akibzaman424@gmail.com>
2024-06-08 04:28:42 -0600

    save point

--- app/src/main/java/com/example/kalendo/ui/component/homescreen/ContentHome.kt
+++ app/src/main/java/com/example/kalendo/ui/component/homescreen/ContentHome.kt
@@ -3,11 +3,13 @@ package com.example.kalendo.ui.component.homescreen
 
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,15 +18,19 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.kalendo.ui.viewmodel.CalendarViewModel
 import com.example.kalendo.domain.model.ImageBannerModel
+import com.example.kalendo.domain.model.MonthModel
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.collectLatest
 import java.util.Calendar
@@ -49,10 +55,7 @@ fun ContentHome(modifier: Modifier = Modifier, viewModel: CalendarViewModel = hi
     ) {
         Log.i("CurrentIndex", scrollState.firstVisibleItemIndex.toString())
         items(months) {month ->
-            BannerHeader(year = month.year, month = month.month, imageBanner = month.banner)
-            for (day in month.days) {
-                DayItem(date = day.date, dayOfWeek = day.dayOfWeek)
-            }
+            MonthItem(month = month)
         }
     }
 
@@ -61,13 +64,13 @@ fun ContentHome(modifier: Modifier = Modifier, viewModel: CalendarViewModel = hi
             .map { it }
             //.debounce(100)  // Debounce to avoid rapid triggering
             .collectLatest { (firstVisibleItemIndex, firstVisibleItemScrollOffset) ->
-                if (firstVisibleItemIndex <= 3 ) {
+                if (firstVisibleItemIndex <= 1 ) {
                     // User has scrolled to the top, load previous month
                     scrollState.scrollToItem( firstVisibleItemIndex + 1, firstVisibleItemScrollOffset)
                     viewModel.loadPreviousMonth()
 
 
-                } else if (firstVisibleItemIndex >=  (scrollState.layoutInfo.totalItemsCount - 3)) {
+                } else if (firstVisibleItemIndex >=  (scrollState.layoutInfo.totalItemsCount - 1)) {
                     // User has scrolled to the bottom, load next month
                     viewModel.loadNextMonth()
 
@@ -82,6 +85,7 @@ private fun BannerHeader(year: Int, month: String, imageBanner: ImageBannerModel
         modifier = Modifier
             .fillMaxWidth()
             .clipToBounds() // Ensures the content doesn't overflow the bounds
+            .padding(top = 30.dp)
     ) {
         if (imageBanner != null) {
             Image(
@@ -97,7 +101,7 @@ private fun BannerHeader(year: Int, month: String, imageBanner: ImageBannerModel
             text = "$month $year",
             modifier = Modifier
                 .align(Alignment.TopStart)
-                .padding(horizontal = 50.dp, vertical = 20.dp),
+                .padding(horizontal = 40.dp, vertical = 25.dp),
             fontWeight = FontWeight.Medium,
             fontSize = 20.sp,
             color = MaterialTheme.colorScheme.background
@@ -107,18 +111,91 @@ private fun BannerHeader(year: Int, month: String, imageBanner: ImageBannerModel
 
 @Composable
 private fun DayItem(date: Int, dayOfWeek: String) {
-    Box (
+    val currentDate = Calendar.getInstance().get(Calendar.DATE)
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 5.dp)
-            .clickable {  }
-    ){
-        Text(
-            text = "$dayOfWeek, $date",
+    ) {
+        Column(
             modifier = Modifier
-                .padding(8.dp)
                 .align(Alignment.Center)
-        )
+                .clickable { }
+                .padding(horizontal = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (date != currentDate) {
+                // Day of the week
+                Text(
+                    text = dayOfWeek.take(3),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 12.sp,
+                )
+                // Date
+                Text(
+                    text = "$date",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                )
+
+            } else {
+                // Current Date/Day
+                Text(
+                    text = dayOfWeek.take(3),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontSize = 12.sp,
+                )
+                Box(
+                    modifier = Modifier
+                        .size(35.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.secondary), // Customize the color as needed
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "$date",
+                        color = MaterialTheme.colorScheme.background,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                    )
+                }
+            }
+
+        }
     }
 }
 
+@Composable
+private fun MonthItem(month: MonthModel){
+
+    var count = 0
+    BannerHeader(year = month.year, month = month.month, imageBanner = month.banner)
+    Row (
+        modifier = Modifier
+            .padding(top = 8.dp, start = 50.dp, end = 50.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Text(
+            text = "To-Do",
+        )
+        Text(
+            text = "Deadline",
+        )
+    }
+    for (day in month.days) {
+        DayItem(date = day.date, dayOfWeek = day.dayOfWeek)
+        count ++
+        if (count != month.days.size) {
+            HorizontalDivider(
+                thickness = 1.dp,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.padding(horizontal = 15.dp))
+        }
+
+    }
+
+}
